8105fe319006db0e5a86a49716c7b50a90957e34
Saheed <thimmoh@gmail.com>
2022-10-15 01:05:33 -0700

    i

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
- *Description: print number 0 to 9
- *Return: 0 (success)
+ * main - prints all strings from 0 to 9
+ * Return: Always 0
  */
 int main(void)
 {
