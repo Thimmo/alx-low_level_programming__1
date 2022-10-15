0a536b0aadcdab76e9a3bf32d591f5acfeb5c72c
Saheed <thimmoh@gmail.com>
2022-10-15 02:53:09 -0700

    new

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -prints combination of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 48; num1 < 56; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 57; num2++)
+		{
+			putchar((char)num1);
+			putchar((char)num2);
+			if (num != 56)
+			{
+				putchar(',')
+				putchar(' ')
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
